5123c8588a5bb76349ef09d8d1266fe15773332d
LNSSPsd <24724395+LNSSPsd@users.noreply.github.com>
2020-08-13 11:36:02 +0800

    + Storylines

--- HelpMeOutPlz.js
+++ HelpMeOutPlz.js
@@ -4,8 +4,7 @@
 const fs=require("fs");
 const vm=require("vm");
 
-const DEBUG = true;
-let initial = 0;
+let DEBUG = true;
 
 let inputcb;
 process.stdin.on("data",(input)=>{
@@ -18,24 +17,32 @@ function log(content){
 	}
 }
 
-function readGameState() {
-	//游戏自动存档，先挖个坑
+// 存档 GAMESTATE
+
+// (以下方法在storyScript中不能使用)
+// ENGINE WIDE readGameStateWithEpid:
+// string lid: 故事线ID
+// return: unsigned short gameState
+// return: ^ 如未找到则返回0.
+function readGameStateWithLineId(lid) {
+	
 }
-function setGameState() {
+// ENGINE WIDE readGameStateWithEpid:AndState:
+// string lid: 故事线ID
+// string epid: 章节ID
+// unsigned short state: Game State.
+// ^ ~建议将0作为起始state.
+function saveGameStateWithLineIdAndState(lid,epid,state) {
+	
 }
+
+// END OF GAMESTATE
+
 function sleep(numberMillis) {
-	//延时方法 (await plz)
+	//延时方法 (await)
 	return new Promise((ret)=>{
 		setTimeout(ret,numberMillis);
 	});
-	//DEPRECATED ORIGINAL METHOD
-	var now = new Date();
-	var exitTime = now.getTime() + numberMillis;
-	while (true) {
-		now = new Date();
-		if (now.getTime() > exitTime)
-			return;
-	}
 }
 
 function minify(json) {
@@ -93,25 +100,67 @@ function fastParse(filename){
 	return JSON.parse(minify(fs.readFileSync(filename)));
 }
 
-let allEpisodes={};
-const mainEpisodeID="helpmeout.mainepisodes.1";
+let allLines={}
 
+for(let episode of fs.readdirSync("storylines")){
+	if(!fs.existsSync(`storylines/${episode}/manifest.json`)){
+		log(`Storyline named ${episode} doesn't have a manifest file,ignoring.`);
+		continue;
+	}
+	const episodeConfig=fastParse(`storylines/${episode}/manifest.json`);
+	if(allLines.hasOwnProperty(episodeConfig.lineID)){
+		log(`ERROR| Duplicated Storyline ID: ${episodeConfig.lineID}`);
+		process.exit(5);
+	}
+	episodeConfig.episodes_dir=episodeConfig.episodes_dir.replace(/\.\./g,"");
+	if(!fs.existsSync(`storylines/${episode}/${episodeConfig.episodes_dir}`)){
+		log(`WARNING| Episodes directory not found in storyline w/ id ${episodeConfig.lineID}, ignoring.`);
+		continue;
+	}
+	let episodes={};
+	for(let ep of fs.readdirSync(`storylines/${episode}/${episodeConfig.episodes_dir}`)){
+		if(!fs.existsSync(`storylines/${episode}/${episodeConfig.episodes_dir}/${ep}/config.json`)){
+			log(`Episode named ${ep} doesn't have a config file,ignoring.`);
+			continue;
+		}
+		const epConfig=fastParse(`storylines/${episode}/${episodeConfig.episodes_dir}/${ep}/config.json`);
+		if(episodes.hasOwnProperty(epConfig.episodeID)){
+			log(`ERROR(IGN)| Duplicated Episode ID ${epConfig.episodeID} in storyline with id: ${episodeConfig.lineID}, IGNORING.`);
+			continue;
+		}
+		if(!fs.existsSync(`storylines/${episode}/${episodeConfig.episodes_dir}/${ep}/${epConfig.storyFile}`)){
+			log(`WARNING| Story file of episode with id ${epConfig.episodeID} not found,ignoring.`);
+			continue;
+		}
+		episodes[epConfig.episodeID]={info:epConfig,script:fs.readFileSync(`storylines/${episode}/${episodeConfig.episodes_dir}/${ep}/${epConfig.storyFile}`).toString()};
+	}
+	if(Object.keys(episodes).length==0){
+		log(`WARNING| Storyline w/ id ${episodeConfig.lineID} does not have any episode, ignoring.`);
+		continue;
+	}
+	if(!episodes.hasOwnProperty(episodeConfig.entry)){
+		log(`WARNING| Entry episode of storyline with id ${episodeConfig.lineID} not found, ignoring this storyline.`);
+		continue;
+	}
+	allLines[episodeConfig.lineID]={info:episodeConfig,episodes};
+}
+/*
 for(let episode of fs.readdirSync("episodes")){
 	if(!fs.existsSync(`episodes/${episode}/config.json`)){
 		log(`Episode named ${episode} doesn't have a config file,ignoring.`);
 		continue;
 	}
 	const episodeConfig=fastParse(`episodes/${episode}/config.json`);
-	if(allEpisodes.hasOwnProperty(episodeConfig.info.episodeID)){
-		log(`ERROR| Duplicated Episode ID: ${episodeConfig.info.episodeID}`);
+	if(allEpisodes.hasOwnProperty(episodeConfig.episodeID)){
+		log(`ERROR| Duplicated Episode ID: ${episodeConfig.episodeID}`);
 		process.exit(5);
 	}
-	if(!fs.existsSync(`episodes/${episode}/${episodeConfig.info.storyFile}`)){
-		log(`WARNING| Story file of episode with id ${episodeConfig.info.episodeID} not found,ignoring.`);
+	if(!fs.existsSync(`episodes/${episode}/${episodeConfig.storyFile}`)){
+		log(`WARNING| Story file of episode with id ${episodeConfig.episodeID} not found,ignoring.`);
 		continue;
 	}
-	allEpisodes[episodeConfig.info.episodeID]={info:episodeConfig.info,settings:episodeConfig.settings,script:fs.readFileSync(`episodes/${episode}/${episodeConfig.info.storyFile}`).toString()};
-}
+	allEpisodes[episodeConfig.episodeID]={info:episodeConfig,script:fs.readFileSync(`episodes/${episode}/${episodeConfig.storyFile}`).toString()};
+}*/
 //story methods
 async function story_say(text, spend, delay, skiable, wrap) {
 	//James标准输出方法
@@ -135,7 +184,7 @@ async function story_say(text, spend, delay, skiable, wrap) {
 	if (wrap == 1) {
 		process.stdout.write("\n");
 	}
-	sleep(delay);
+	await sleep(delay);
 }
 
 async function story_askforhelp(cb){
@@ -154,34 +203,78 @@ async function story_askforhelp(cb){
 }
 //story methods =END=
 
-function runEpisodeWithEpisodeId(epid){
-	if(!allEpisodes.hasOwnProperty(epid)){
-		log(`ERROR| Next episode(id:${epid}) not found,nowhere to go..`);
-		process.exit(6);
+function runStoryline(storyline){
+	let allEpisodes=storyline.episodes;
+	function runEpisodeWithEpisodeId(epid){
+		if(!allEpisodes.hasOwnProperty(epid)){
+			log(`ERROR| Next episode(id:${epid}) not found,nowhere to go..`);
+			process.exit(6);
+		}
+		try{
+			vm.runInNewContext(allEpisodes[epid].script,{engine:{
+				log:log,
+				hasEpisode:allEpisodes.hasOwnProperty,
+				jumpToEpisode:runEpisodeWithEpisodeId,
+				sleepNoAwait:(numberMillis,noDeprecationWarning)=>{
+					// DEPRECATED , USE (await) game.sleep instead.
+					if(!noDeprecationWarning)log(`engine.sleepNoAwait is deprecated,please use await game.sleep instead.`);
+					let now = new Date();
+					let exitTime = now.getTime() + numberMillis;
+					while (true) {
+						now = new Date();
+						if (now.getTime() > exitTime)
+							return;
+					}
+				},
+				finish:()=>{
+					//完成所有章节
+					//目前无更多操作 故退出.
+					//(结束故事线)
+					process.exit(0);
+				},
+				fail:(reason)=>{
+					//游戏失败
+					//目前无更多操作 故退出.
+					console.log(`Game failed due to ${reason}.`);
+					process.exit(1);
+				}
+			},game:{
+				sleep:sleep
+			},story:{
+				say:story_say,
+				askforhelp:story_askforhelp
+			},setInterval,setTimeout,Promise},{filename:`${allEpisodes[epid].info.episodeID}.${allEpisodes[epid].info.storyFile}`});
+		}catch(err){
+			log(`ERROR| SCRIPT ERROR OCCURRED WHEN EXECUTING STORY SCRIPT:${err.stack}`);
+			process.exit(7);
+		}
 	}
-	try{
-		vm.runInNewContext(allEpisodes[epid].script,{engine:{
-			log:log,
-			hasEpisode:allEpisodes.hasOwnProperty,
-			jumpToEpisode:runEpisodeWithEpisodeId,
-			finish:()=>{
-				//完成所有章节
-				//目前无更多操作 故退出.
-				process.exit(0);
-			},
-			fail:(reason)=>{
-				//游戏失败
-				//目前无更多操作 故退出.
-				console.log(`Game failed due to ${reason}.`);
-				process.exit(1);
-			}
-		},story:{
-			say:story_say,
-			askforhelp:story_askforhelp
-		},setInterval,setTimeout,Promise},{filename:`${allEpisodes[epid].info.episodeID}.${allEpisodes[epid].info.storyFile}`});
-	}catch(err){
-		log(`ERROR| SCRIPT ERROR OCCURRED WHEN EXECUTING STORY SCRIPT:${err.stack}`);
-	}
+	runEpisodeWithEpisodeId(storyline.info.entry);
 }
 
-runEpisodeWithEpisodeId(mainEpisodeID);
+async function storyLineSelector(){
+	console.log("Select a story line");
+	console.log("===================");
+	console.log("0) Exit");
+	let ltd=[0];
+	for(let line in allLines){
+		console.log("%d) %s",ltd.length,allLines[line].info.lineName);
+		ltd.push(line);
+	}
+	let selLine;
+	while(true){
+		process.stdout.write("(ID): ");
+		let id=await new Promise((retr)=>{
+			inputcb=retr;
+		});
+		if(ltd[parseInt(id)]===undefined)continue;
+		if(parseInt(id)==0){
+			process.exit(0);
+		}
+		selLine=allLines[ltd[parseInt(id)]];
+		break;
+	}
+	runStoryline(selLine);
+}
+storyLineSelector();
+//runEpisodeWithEpisodeId(mainEpisodeID);
